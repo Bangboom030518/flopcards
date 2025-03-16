3a62c0e6d8b8032e04765e6482953782a9466a93
Charlie Clift <bang.boom030518@gmail.com>
2025-03-16 13:40:37 +0000

    fix: remove outdated db ui

--- src/main.rs
+++ src/main.rs
@@ -15,56 +15,7 @@ use tokio::net::TcpListener;
 mod components;
 mod data;
 
-fn create_set_popup(subjects: &[Subject]) -> Dialog {
-    dialog()
-        .class("bg-transparent max-w-[60ch] w-full inset-0 m-auto")
-        .id("create-set-dialog")
-        .child(
-            div()
-                .class("card inset-0 relative")
-                .child(
-                    button("close-popup")
-                        .class("w-[5ch] h-[5ch] top-2 right-2 absolute sound-stop-baby")
-                        .hx_on("click", "this.parentElement.parentElement.close()")
-                        .child(
-                            img("/assets/close.svg", "close")
-                                .size(24, 24)
-                                .class("w-full h-full"),
-                        ),
-                )
-                .child(h2("create set"))
-                .child(
-                    form(FormMethod::Post, "create-set")
-                        .class("w-full grid gap-4")
-                        .child(components::text_input(
-                            "set-title",
-                            "title",
-                            "set title",
-                            InputType::Text,
-                            true,
-                            None,
-                        ))
-                        .child(
-                            textarea("description-input", "description")
-                                .placeholder("description")
-                                .class("text-black"),
-                        )
-                        .child(
-                            select("subject-input", "subject")
-                                .options(subjects.iter().map(|subject| {
-                                    (subject.name.to_string(), subject.name.replace(' ', "-"))
-                                }))
-                                .class("text-black"),
-                        )
-                        .child(
-                            components::button_with_icon("create-set", "create", "create set")
-                                .class("input-accent"),
-                        ),
-                ),
-        )
-}
-
-async fn index(request: Request<hyper::body::Incoming>) -> Result<Html, ResourceError> {
+fn index(request: Request<hyper::body::Incoming>) -> Result<Html, ResourceError> {
     let subjects = Subject::fetch_all();
     let query = Query::from_request(&request);
     let sets = if let Ok(subject) = query.get("subject") {
@@ -72,7 +23,7 @@ async fn index(request: Request<hyper::body::Incoming>) -> Result<Html, Resource
     } else {
         Vec::new()
     };
-    Err(html("en")
+    Ok(html("en")
         .child(
             head()
                 .template()
@@ -86,31 +37,19 @@ async fn index(request: Request<hyper::body::Incoming>) -> Result<Html, Resource
                 .child(h1("flopcards"))
                 .child(components::subject_menu(&subjects))
                 .child(components::set_list(&sets))
-                .child(components::fab_dropdown(
-                    "create",
-                    "create",
-                    ["set", "folder"].into_iter().map(|item| {
-                        components::button_with_icon(format!("create-{item}"), item, item).onclick(
-                            format!(
-                                "javascript:document.getElementById('create-{item}-dialog').show()"
-                            ),
-                        )
-                    }),
-                ))
-                .child(create_set_popup(&subjects))
                 .child(components::loading_animation())
                 .script(include_str!("../script.js")),
         ))
 }
 
-async fn sets_view(request: Request<hyper::body::Incoming>) -> Result<Section, ResourceError> {
+fn sets_view(request: Request<hyper::body::Incoming>) -> Result<Section, ResourceError> {
     let request = &request;
     let query = Query::from_request(request);
     let subject = query.get("subject")?;
     Ok(components::set_list(&Set::fetch_all(&subject)?))
 }
 
-async fn set(path: &str) -> Result<Html, ResourceError> {
+fn set(path: &str) -> Result<Html, ResourceError> {
     let set = Set::get(path).unwrap_or_else(|_| todo!("404"));
     Ok(html("en")
         .child(
@@ -136,7 +75,7 @@ async fn router(
     match *request.method() {
         Method::GET => {
             if path == "/" {
-                index(request).await.response_ok()
+                index(request)?.response_ok()
             } else if path == "/favicon.ico" {
                 let path = format!("/{}/assets/favicon.ico", env!("CARGO_MANIFEST_DIR"));
                 let bytes = fs::read(path)
@@ -165,13 +104,11 @@ async fn router(
                 return Ok(response);
             } else if let Some(path) = path.strip_prefix("/view/") {
                 match path {
-                    "sets" => sets_view(request).await?.response_ok(),
+                    "sets" => sets_view(request)?.response_ok(),
                     _ => Err(ResourceError::NotFound(format!("/view/{path}"))),
                 }
             } else if let Some(path) = path.strip_prefix("/sets/") {
-                set(path).await?.response_ok()
-            } else if let Some(path) = path.strip_prefix("/edit-set/") {
-                edit_set(path).await?.response_ok()
+                set(path)?.response_ok()
             } else {
                 Err(ResourceError::NotFound(path.to_string()))
             }
